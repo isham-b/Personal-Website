bd171852b6f210561d36383782705c1c067d6c54
isham-b <66917399+isham-b@users.noreply.github.com>
2021-07-12 03:01:33 -0400

    Completed mobile navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,41 @@
-/** */
+/*---------------------------- NAV MENU --------------------------------- */
+{
+    const navMenu = document.getElementById("nav_menu");
+    const navToggle = document.getElementById("nav_toggle");
+    const navClose = document.getElementById("nav_close");
+    const navLink = document.querySelectorAll(".nav_link");
+
+    // Reveal Menu (mobile)
+    if (navToggle) {
+        navToggle.addEventListener("click", () => {
+            navMenu.classList.add("show_menu");
+        })
+    }
+    
+
+    // Close Menu (mobile)
+    if (navClose) {
+        navClose.addEventListener("click", () => {
+            navMenu.classList.remove("show_menu");
+        })
+    }
+
+
+    // Collapse Menu on click (mobile)
+    navLink.forEach((l) => {
+        l.addEventListener("click", () => {
+            navMenu.classList.remove("show_menu");
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+}
