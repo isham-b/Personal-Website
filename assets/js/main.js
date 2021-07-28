c9887a34f39d9fdda68343e5b9591c2feba79e92
isham-b <66917399+isham-b@users.noreply.github.com>
2021-07-27 20:35:06 -0400

    Created "qualifications" section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-/*---------------------------- NAV MENU --------------------------------- */
 {
+    /*---------------------------- NAV MENU --------------------------------- */
     const navMenu = document.getElementById("nav_menu");
     const navToggle = document.getElementById("nav_toggle");
     const navClose = document.getElementById("nav_close");
@@ -29,7 +29,24 @@
     })
 
 
+    /*---------------------------- SKILLS MENU --------------------------------- */
+    const skillsContent = document.getElementsByClassName("skills_content");
+    const skillsHeader = document.querySelectorAll(".skills_header");
 
+    function toggleSkills() {
+        let itemClass = this.parentNode.className
+
+        for (let i = 0; i < skillsContent.length; i++) {
+           skillsContent[i].className = "skills_content skills_close"
+        }
+        if (itemClass === "skills_content skills_close") {
+            this.parentNode.className = "skills_content skills_open"
+        }
+    }
+
+    skillsHeader.forEach((e) => {
+        e.addEventListener("click", toggleSkills)
+    })
 
 
 
