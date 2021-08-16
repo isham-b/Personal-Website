be82e536069e2fa375b0e472711665a45e0e171d
isham-b <66917399+isham-b@users.noreply.github.com>
2021-08-15 20:30:27 -0400

    Updated scroll behaviour

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,10 +120,45 @@
       });
 
 
+    /*---------------------------- SCROLL SECTIONS ACTIVE LINK --------------------------------- */
+    const sections = document.querySelectorAll('section[id]')
+
+    function scrollActive(){
+        const scrollY = window.pageYOffset
+    
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight
+            const sectionTop = current.offsetTop - 50;
+            sectionId = current.getAttribute('id')
+    
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active_link')
+            }else{
+                document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active_link')
+            }
+        })
+    }
+    window.addEventListener('scroll', scrollActive)
 
 
 
+    /*==================== ADJUSTABLE NAVBAR SHADOW ====================*/ 
+    function scrollHeader(){
+        const nav = document.getElementById('header')
+        // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+        if(this.scrollY >= 80) nav.classList.add('scroll_header'); else nav.classList.remove('scroll_header')
+    }
+    window.addEventListener('scroll', scrollHeader)
 
+
+   
+    /*---------------------------- SHOW SCROLL TO TOP --------------------------------- */ 
+    function scrollUp(){
+        const scroll_up = document.getElementById('scroll_up');
+        // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+        if(this.scrollY >= 560) scroll_up.classList.add('show_scroll'); else scroll_up.classList.remove('show_scroll')
+    }
+    window.addEventListener('scroll', scrollUp)
 
 }
 
